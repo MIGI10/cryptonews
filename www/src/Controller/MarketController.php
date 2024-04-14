68b4b01099ee853a1f74999e673cb4e8be04935b
Miguel-Nicolás Palau Lessa <miguelnicolas.palau@students.salle.url.edu>
2024-04-14 15:45:20 +0200

    add market twig

--- www/src/Controller/MarketController.php
+++ www/src/Controller/MarketController.php
@@ -7,6 +7,9 @@ namespace Salle\Ca2CryptoNews\Controller;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Psr\Http\Message\ResponseInterface as Response;
 use Slim\Views\Twig;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Error\SyntaxError;
 
 final class MarketController
 {
@@ -19,9 +22,25 @@ final class MarketController
 
     public function apply(Request $request, Response $response): Response
     {
+        $balance = '';
+        if (isset($_SESSION['user'])) {
+            $balance = $_SESSION['user']->getNumBitcoins();
+        }
 
-        return $this->twig->render($response, 'market.twig', [
-            'user' => $user
-        ]);
+        $currencies = [];
+        $currencies[]['name'] = 'Bitcoin';
+        $currencies[]['price'] = 'EUR 60.111,24';
+
+        $currencies[]['name'] = 'Ethereum';
+        $currencies[]['price'] = 'EUR 2.833,59';
+
+        try {
+            return $this->twig->render($response, 'market.twig', [
+                'balance' => $balance,
+                'currencies' => $currencies
+            ]);
+        } catch (LoaderError | RuntimeError| SyntaxError $e) {
+            return $response->withStatus(500);
+        }
     }
 }
